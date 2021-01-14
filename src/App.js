c3eaf8f9886e8b7774438799aa7eaedf17a5cbdb
samynaj <samuelnnj@gmail.com>
2021-01-14 20:48:57 +0100

    added movie search bar and header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1 className="app-header">The Shoppies</h1>
+      <div className="app-search">
+        <form className="app-search-form">
+          <label className="app-search-form-label">Movie Title</label>
+          <input className="app-search-form-input" type="text" placeholder="Search movie name" />
+          <button className="app-search-form-button" type="submit">Search</button>
+        </form>
+      </div>
     </div>
   );
 }
