35b8e2a3b4b6c1f941debfd453831f6941e8d67d
samynaj <samuelnnj@gmail.com>
2021-01-17 00:09:15 +0100

    added the UI for movie card

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import React, { useState, useEffect} from 'react'
 import './App.css';
 
 function App() {
+
+
   return (
     <div className="app">
       <h1 className="app-header">The Shoppies</h1>
@@ -10,6 +13,25 @@ function App() {
           <input className="app-search-form-input" type="text" placeholder="Search movie name" />
           <button className="app-search-form-button" type="submit">Search</button>
         </form>
+      </div>
+      <div className="app-body">
+        <div className="app-body-cardlist">
+          <h3>Results for</h3>
+          <div className="app-body-cardlist-card">
+            <h5>Movie title</h5>
+            <p>year</p>
+            <button>Nominate</button>
+          </div>
+        </div>
+        <div className="app-body-nominate">
+          <h3>Movies Nominated</h3>
+          <div>
+            <h5>Movie title</h5>
+            <p>year</p>
+            <button>Remove</button>
+          </div>
+          
+        </div>
       </div>
     </div>
   );
